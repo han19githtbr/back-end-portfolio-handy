8aa8cdb1e15ca6f005e6d6cdfb38d650cef5fd9b
han19githtbr <milliance23@gmail.com>
2024-08-24 13:35:02 -0300

    curriculo atualizado

--- src/main/java/br/com/portfoliohandy/portfoliohandy/controllers/PortfolioController.java
+++ src/main/java/br/com/portfoliohandy/portfoliohandy/controllers/PortfolioController.java
@@ -141,13 +141,13 @@ public class PortfolioController {
 	private String obterNomeArquivo(String idioma) {
 		switch (idioma) {
 			case "pt-br":
-				return "CV_HANDY_BR.pdf";
+				return "CV_ATUALIZADO_HANDY_BR.pdf";
 			case "en":
-				return "CV_HANDY_EN.pdf";
+				return "CV_ATUALIZADO_HANDY_EN.pdf";
 			case "fr":
-				return "CV_HANDY_FR.pdf";
+				return "CV_ATUALIZADO_HANDY_FR.pdf";
 			default:
-				return "CV_HANDY_BR.pdf";
+				return "CV_ATUALIZADO_HANDY_BR.pdf";
 		}
 	}
 
